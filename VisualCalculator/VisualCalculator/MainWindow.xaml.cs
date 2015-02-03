2e016601a18c32acc41c8a3484f878098628212d
Daniel Curtis <mrjoefriday@gmail.com>
2015-02-03 11:10:01 -0500

    updates

--- VisualCalculator/VisualCalculator/MainWindow.xaml.cs
+++ VisualCalculator/VisualCalculator/MainWindow.xaml.cs
@@ -20,18 +20,64 @@ namespace VisualCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double operand;
+        private string operation;
+
         public MainWindow()
         {
+            reset();
             InitializeComponent();
+        }
+
+        private void reset()
+        {
+            operand = 0.0;
+            operation = "";
         }
 
         private void btn_Click(object sender, RoutedEventArgs e)
         {
             Button clickedButton = (Button)sender;
             string buttonName = clickedButton.Name;
-            string[] actionButtonNames = {"btnMultiply", "btnDivide", "btnAdd","btnSubtract","btnClear","btnCalculate"};
+            string[] actionButtonNames = {"btnMultiply", "btnDivide", "btnAdd","btnSubtract"};
             if (actionButtonNames.Contains(buttonName))
             {
+                operand = Double.Parse(txtResult.Text);
+                operation = buttonName;
+                txtResult.Text = "0";
+            }
+            else if( buttonName == "btnCalculate")
+            {
+                txtResult.Text = calculateResult().ToString();
+            }
+            else if(buttonName == "btnClear")
+            {
+                reset();
+                txtResult.Text = "0";
+            }
+            else
+            {
+                txtResult.Text += clickedButton.Content.ToString();
+            }
+        }
+
+        private double calculateResult()
+        {
+            if(operation == "btnAdd")
+            {
+                return operand + Double.Parse(txtResult.Text);
+            }
+            else if(operation == "btnSubtract")
+            {
+                return operand - Double.Parse(txtResult.Text);
+            }
+            else if(operation == "btnMultiply")
+            {
+                return operand * Double.Parse(txtResult.Text);
+            }
+            else
+            {
+                return operand / Double.Parse(txtResult.Text);
             }
         }
     }
